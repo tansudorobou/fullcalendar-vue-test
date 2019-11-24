12b394d677ea6566aa9269ced1db269f3ec624a2
hishiyuu <tansudorobou@gmail.com>
2019-11-24 17:38:37 +0900

    first commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,43 @@
+<template>
+  <FullCalendar
+    :schedulerLicenseKey="calendarCustomize.schedulerLicenseKey"
+    :plugins="calendarCustomize.plugins"
+    :defaultView="calendarCustomize.defaultView"
+    :height="calendarCustomize.height"
+    :resources="calendarCustomize.events"
+    :slotDuration="calendarCustomize.slotDuration"
+  />
+</template>
+
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
+
+export default {
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarCustomize: {
+        schedulerLicenseKey: "GPL-My-Project-Is-Open-Source",
+        plugins: [resourceTimelinePlugin],
+        defaultView: "resourceTimelineDay",
+        height: "auto",
+        slotDuration: { days: 1 },
+        resources:
+          "https://fullcalendar.io/demo-resources.json?with-nesting&with-colors",
+        // Resourcesごとのイベントのデモデータ
+        events:
+          "https://fullcalendar.io/demo-events.json?single-day&for-resource-timeline"
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/timeline/main.css";
+@import "~@fullcalendar/resource-timeline/main.css";
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <calendar />
   </div>
 </template>
+
+<script>
+import calendar from "../components/Calendar.vue";
+export default {
+  components: {
+    calendar
+  }
+};
+</script>
